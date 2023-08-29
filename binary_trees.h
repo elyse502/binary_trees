a22f20d52af61685a0f6dcc1863ca78ad41a0d0f
elyse502 <elyseniyibizi502@gmail.com>
2023-08-29 00:05:21 -0700

    Builds an AVL tree from an array

--- binary_trees.h
+++ binary_trees.h
@@ -82,5 +82,6 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+void sata_helper(avl_t **root, int *array, size_t lo, size_t hi);
 
 #endif /* BINARY_TREES_H */
